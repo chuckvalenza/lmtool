52087276d80e8181dd8c6cc85ef14a71856d2e07
Chuck Valenza <fallencx@gmail.com>
2016-04-25 21:56:42 -0400

    Write basic structure of lp_transform
    Add unimplemented get_cell which will return the cooresponding lp_cell for the
    pixel being evaluated.

--- src/lpt.c
+++ src/lpt.c
@@ -8,7 +8,7 @@
 #include <math.h>
 
 /**
- * malloc a log-polar grid
+ * malloc a log-polar grid and initialize all values
  */
 struct lp_cell** allocate_lparray(long width, long height)
 {
@@ -16,6 +16,14 @@ struct lp_cell** allocate_lparray(long width, long height)
 
     for (int i = 0; i < width; i++) {
         lparr[i] = malloc(sizeof(struct lp_cell) * height);
+        lparr[i]->cent = malloc(sizeof(struct pixel));
+        lparr[i]->cent->x = 0;
+        lparr[i]->cent->y = 0;
+        lparr[i]->dist = 0;
+        lparr[i]->npixels = 0;
+        lparr[i]->r = 0;
+        lparr[i]->g = 0;
+        lparr[i]->b = 0;
     }
 
     return lparr;
@@ -28,30 +36,39 @@ struct lp_cell** allocate_lparray(long width, long height)
 void lp_transform(unsigned char* image_data, struct lp_cell* **lp_arr,
     unsigned long byte_count, long in_w, long in_h, long out_w, long out_h)
 {
+    struct pixel* cur_p = NULL;
     // center pixel of the image
     struct pixel* cent_p = malloc(sizeof(struct pixel));
     cent_p->x = in_w / 2;
     cent_p->y = in_h / 2;
 
     // only check if within largest circle
-    for (int row = 0; row < in_w; row++) {
-        for (int col = 0; col < in_h; col++) {
-            struct pixel* cur_p = malloc(sizeof(struct pixel));
+    for (int row = 0; row < in_h; row++) {
+        for (int col = 0; col < in_w; col++) {
+            cur_p = malloc(sizeof(struct pixel));
             cur_p->x = col;
             cur_p->y = row;
 
             // don't do anything unless in the radius of our overlayed circle
             if (in_circ(cur_p, cent_p, in_w / 2)) {
-                fprintf(stderr, "computing for pixel in overlay\n");
-            } else {
-                fprintf(stderr, "discarding pixel out of overlay\n");
+                // get the cell which the pixel belongs to
+                struct lp_cell* cur_cell = get_cell(cur_p);
+
+                // if the current pixel is in the circle approx of the current 
+                // cell add it to the grey value of that current cell.
+                if (in_circ(cur_p, cur_cell->cent, cur_cell->radius)) {
+                    cur_cell->npixels++;
+                    cur_cell->r += cur_p->r;
+                    cur_cell->g += cur_p->g;
+                    cur_cell->b += cur_p->b;
+                }
             }
         }
     }
 }
 
 /**
- * check if the pixel is within the range of our overlay image
+ * Check if the pixel is within the range of our overlay image
  */
 int in_circ(struct pixel* p, struct pixel* cent, double radius)
 {
@@ -65,5 +82,15 @@ int in_circ(struct pixel* p, struct pixel* cent, double radius)
     } else {
         return 1;
     }
+}
+
+/**
+ * Return the pointer to the corresponding pointer of the current pixel
+ */
+struct lp_cell* get_cell(struct pixel* p)
+{
+    struct lp_cell* lpc;
+
+    return lpc;
 }
 
--- src/lpt.h
+++ src/lpt.h
@@ -8,8 +8,10 @@
 #include <stdlib.h>
 
 struct lp_cell {
-    long npixels;
+    struct pixel* cent;
     double dist; // distance from center is the scaling factor
+    double radius;
+    long npixels;
     long r;
     long g;
     long b;
@@ -18,6 +20,9 @@ struct lp_cell {
 struct pixel {
     long x;
     long y;
+    long r;
+    long g;
+    long b;
 };
 
 struct lp_cell** allocate_lparray(long width, long height);
@@ -26,3 +31,6 @@ void lp_transform(unsigned char* image_data, struct lp_cell* **lp_arr,
     unsigned long byte_count, long in_w, long in_h, long out_w, long out_h);
 
 int in_circ(struct pixel* p, struct pixel* cent, double radius);
+
+struct lp_cell* get_cell(struct pixel* p);
+
